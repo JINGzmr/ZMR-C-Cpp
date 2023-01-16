0ba826dc517db325e76778d465e5d68509ccc8d8
JINGzmr <2743377630@qq.com>
2023-01-16 23:49:10 +0800

    0

--- 运算器.c
+++ 运算器.c
@@ -615,9 +615,60 @@
 //     printf("%c",a);
 // }
 
+// #include<stdio.h>
+// int main()
+// {
+//     if(-1)printf("666");
+//     return 0;
+// }
+
 #include<stdio.h>
-int main()
-{
-    if(-1)printf("666");
-    return 0;
+#include<stdlib.h>
+//  Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      struct ListNode *next;
+ };
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+//排除空表
+    if(list1==NULL)/*不是list1->next=NULL，因为list1指向第一个节点，而非头节点*/
+        return list2;/*list1->next=NULL表示他有且只有第一个节点（也就是有val，像[1]）*/
+    if(list2==NULL)
+        return list1;
+
+//创新表、新指针
+    struct ListNode* list3,*p,*q,*t;
+    list3=(struct ListNode*)malloc(sizeof(struct ListNode));
+    list3->next=NULL;
+    p=list1;
+    q=list2;
+    t=list3;
+
+//串
+    while(p!=NULL&&q!=NULL)
+    {
+        if(p->val < q->val){
+            t->next=p;
+            // t->val=p->val;
+            p=p->next;
+        }
+        else{
+            t->next=q;
+            // t->val=q->val;
+            q=q->next;
+        }
+        t=t->next;
+        t->next=NULL;//不要忘
+    }
+
+//其中一表空了，直接把另一表剩下的全部给list3
+    if(p){
+        t->next=p;
+    }
+    if(q){
+        t->next=q;
+    }
+
+    return list3->next;//如果是list3,则会把list3->val中的乱码输出
 }
