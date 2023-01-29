d30833863f2516dd7f43a4999fcd7c98ab070f95
JINGzmr <2743377630@qq.com>
2023-01-29 14:45:26 +0800

    0

--- 数据结构与算法/力扣/线性表.c
+++ 数据结构与算法/力扣/线性表.c
@@ -4,7 +4,7 @@
 //  Definition for singly-linked list.
   struct ListNode {
       int val;
-      struct ListNode *next;
+      struct ListNode *next; 
  };
  
 // struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
