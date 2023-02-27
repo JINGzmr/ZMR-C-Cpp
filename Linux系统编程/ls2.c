f90e2aee27bb614c7a257616beef780110638f49
JINGzmr <2743377630@qq.com>
2023-02-27 22:13:37 +0800

    ls

--- Linux系统编程/ls2.c
+++ Linux系统编程/ls2.c
@@ -52,16 +52,17 @@ int g_maxlen;                //存放某目录下最长文件名的长度
 
 void printf_name(char *name,int color); //带有不同颜色的打印文件名
 int get_color(struct stat buf);         //不同文件类型得到不同颜色型号
-void display_dir(int flag_param,char *path);
-//为显示某个目录下的文件做准备
-void display_dir_R(int flag,char *name);
-//递归打开目录
-void display_attribute(struct stat buf,  char *name);
-//打印名为name的文件信息
-void display_single(int flag,struct stat buf,char *name,int color);
-//输出文件的文件名
-void display(int flag,char *pathname);
-//根据命令行参数flag和完整路径名，显示目标文件
+`
+void display_dir(int flag_param,char *path);//为显示某个目录下的文件做准备
+
+void display_dir_R(int flag,char *name);//递归打开目录
+
+void display_attribute(struct stat buf,  char *name);//打印名为name的文件信息
+
+void display_single(int flag,struct stat buf,char *name,int color);//输出文件的文件名
+
+void display(int flag,char *pathname);//根据命令行参数flag和完整路径名，显示目标文件
+
 
 int main(int argc,char  *argv[])
 {
@@ -186,133 +187,133 @@ int main(int argc,char  *argv[])
     }while(i<argc);
     return 0;
 }
-void display_attribute(struct stat buf,  char *name)
-{
-	char buf_time[32];
-	struct passwd *psd;      //从该结构体中获取文件所有者的用户名
-    struct group *grp;       //从该结构体中获取文件所有者所属组的组名
+// void display_attribute(struct stat buf,  char *name)
+// {
+// 	char buf_time[32];
+// 	struct passwd *psd;      //从该结构体中获取文件所有者的用户名
+//     struct group *grp;       //从该结构体中获取文件所有者所属组的组名
 	
-    //获取并打印文件类型
-	if(S_ISLNK(buf.st_mode))
-	{
-		printf("l");
-	}
-	else if(S_ISREG(buf.st_mode))
-	{
-		printf("-");
-	}
-	else if(S_ISDIR(buf.st_mode))
-	{
-		printf("d");
-	}
-	else if(S_ISCHR(buf.st_mode))
-	{
-		printf("c");
-	}
-	else if(S_ISBLK(buf.st_mode))
-	{
-		printf("b");
-	}
-	else if(S_ISFIFO(buf.st_mode))
-	{
-		printf("f");
-	}
-	else if(S_ISSOCK(buf.st_mode))
-	{
-		printf("s");
-	}
+//     //获取并打印文件类型
+// 	if(S_ISLNK(buf.st_mode))
+// 	{
+// 		printf("l");
+// 	}
+// 	else if(S_ISREG(buf.st_mode))
+// 	{
+// 		printf("-");
+// 	}
+// 	else if(S_ISDIR(buf.st_mode))
+// 	{
+// 		printf("d");
+// 	}
+// 	else if(S_ISCHR(buf.st_mode))
+// 	{
+// 		printf("c");
+// 	}
+// 	else if(S_ISBLK(buf.st_mode))
+// 	{
+// 		printf("b");
+// 	}
+// 	else if(S_ISFIFO(buf.st_mode))
+// 	{
+// 		printf("f");
+// 	}
+// 	else if(S_ISSOCK(buf.st_mode))
+// 	{
+// 		printf("s");
+// 	}
 	
-    //获取并打印文件所有者的权限
-	if(buf.st_mode&S_IRUSR)
-	{
-		printf("r");
-	}
-	else
-	{
-		printf("-");
-	}
-	if(buf.st_mode&S_IWUSR)
-	{
-		printf("w");
-	}
-	else
-	{
-		printf("-");
-	}
-	if(buf.st_mode&S_IXUSR)
-	{
-		printf("x");
-	}
-	else
-	{
-		printf("-");
-	}
+//     //获取并打印文件所有者的权限
+// 	if(buf.st_mode&S_IRUSR)
+// 	{
+// 		printf("r");
+// 	}
+// 	else
+// 	{
+// 		printf("-");
+// 	}
+// 	if(buf.st_mode&S_IWUSR)
+// 	{
+// 		printf("w");
+// 	}
+// 	else
+// 	{
+// 		printf("-");
+// 	}
+// 	if(buf.st_mode&S_IXUSR)
+// 	{
+// 		printf("x");
+// 	}
+// 	else
+// 	{
+// 		printf("-");
+// 	}
 	
-    //获取并打印与文件所有者同组的用户对该文件的操作权限
-	if(buf.st_mode&S_IRGRP)
-	{
-		printf("r");
-	}
-	else
-	{
-		printf("-");
-	}
-	if(buf.st_mode&S_IWGRP)
-	{
-		printf("w");
-	}
-	else
-	{
-		printf("-");
-	}
-	if(buf.st_mode&S_IXGRP)
-	{
-		printf("x");
-	}
-	else
-	{
-		printf("-");
-	}
+//     //获取并打印与文件所有者同组的用户对该文件的操作权限
+// 	if(buf.st_mode&S_IRGRP)
+// 	{
+// 		printf("r");
+// 	}
+// 	else
+// 	{
+// 		printf("-");
+// 	}
+// 	if(buf.st_mode&S_IWGRP)
+// 	{
+// 		printf("w");
+// 	}
+// 	else
+// 	{
+// 		printf("-");
+// 	}
+// 	if(buf.st_mode&S_IXGRP)
+// 	{
+// 		printf("x");
+// 	}
+// 	else
+// 	{
+// 		printf("-");
+// 	}
 	
-    //获取并打印其他用户对该文件的操作权限
-	if(buf.st_mode&S_IROTH)
-	{
-		printf("r");
-	}
-	else
-	{
-		printf("-");
-	}
-	if(buf.st_mode&S_IWOTH)
-	{
-		printf("w");
-	}
-	else
-	{
-		printf("-");
-	}
-	if(buf.st_mode&S_IXOTH)
-	{
-		printf("x");
-	}
-	else
-	{
-		printf("-");
-	}
-	printf("  ");
+//     //获取并打印其他用户对该文件的操作权限
+// 	if(buf.st_mode&S_IROTH)
+// 	{
+// 		printf("r");
+// 	}
+// 	else
+// 	{
+// 		printf("-");
+// 	}
+// 	if(buf.st_mode&S_IWOTH)
+// 	{
+// 		printf("w");
+// 	}
+// 	else
+// 	{
+// 		printf("-");
+// 	}
+// 	if(buf.st_mode&S_IXOTH)
+// 	{
+// 		printf("x");
+// 	}
+// 	else
+// 	{
+// 		printf("-");
+// 	}
+// 	printf("  ");
 
-//根据uid与gid获取文件所有者的用户名与组名
-    psd = getpwuid(buf.st_uid);
-    grp = getgrgid(buf.st_gid);
-    printf("%4d",buf.st_nlink);  //打印文件的链接数
-    printf("%-8s",psd->pw_name);
-    printf("%-8s",grp->gr_name);
-    printf("%6d",buf.st_size);   //打印文件的大小
-    strcpy(buf_time,ctime(&buf.st_mtime));
-    buf_time[strlen(buf_time)-1]='\0';    //去掉换行符
-    printf(" %s",buf_time);               //打印文件的时间信息
+// //根据uid与gid获取文件所有者的用户名与组名
+//     psd = getpwuid(buf.st_uid);
+//     grp = getgrgid(buf.st_gid);
+//     printf("%4d",buf.st_nlink);  //打印文件的链接数
+//     printf("%-8s",psd->pw_name);
+//     printf("%-8s",grp->gr_name);
+//     printf("%6d",buf.st_size);   //打印文件的大小
+//     strcpy(buf_time,ctime(&buf.st_mtime));
+//     buf_time[strlen(buf_time)-1]='\0';    //去掉换行符
+//     printf(" %s",buf_time);               //打印文件的时间信息
     	
-}
+// }
 void display_single(int flag,struct stat buf,char *name,int color)
 {
     int i,len;
@@ -622,26 +623,26 @@ void display_dir_R(int flag,char *name)
 //     return color;
 // }
 
-void printf_name(char *name,int color)
-{
-    if(color == GREEN)
-    {
-        printf("\033[1m\033[32m%-s\033[0m",name);
-    }
-    else if(color == BLUE)
-    {
-        printf("\033[1m\033[34m%-s\033[0m",name);
-    }
-    else if(color == WHITE)
-    {
-        printf("%-s",name);
-    }
-    else if(color == LBLUE)
-    {
-        printf("\033[1m\033[36m%-s\033[0m",name);
-    }
-    else if(color == YELLOW)
-    {
-        printf("\033[1m\033[33m%-s\033[0m",name);
-    }
-}
+// void printf_name(char *name,int color)
+// {
+//     if(color == GREEN)
+//     {
+//         printf("\033[1m\033[32m%-s\033[0m",name);
+//     }
+//     else if(color == BLUE)
+//     {
+//         printf("\033[1m\033[34m%-s\033[0m",name);
+//     }
+//     else if(color == WHITE)
+//     {
+//         printf("%-s",name);
+//     }
+//     else if(color == LBLUE)
+//     {
+//         printf("\033[1m\033[36m%-s\033[0m",name);
+//     }
+//     else if(color == YELLOW)
+//     {
+//         printf("\033[1m\033[33m%-s\033[0m",name);
+//     }
+// }
--- Linux系统编程/myls.c
+++ Linux系统编程/myls.c
@@ -1,248 +1,434 @@
-#include<stdio.h>
-#include<sys/stat.h>
-#include<dirent.h>
-#include<errno.h> //errno的头文件
-#include<string.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<pwd.h>
-#include<grp.h>
-#include<time.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h> //errno的头文件
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 #include <locale.h>
 #include <langinfo.h>
-//存放文件名的数组，方便a-z排序
+// 存放文件名的数组，方便a-z排序
 char *filenames[4096];
-//目录中文件个数，用来计数    
-int file_cnt = 0;   
+// 目录中文件个数，用来计数
+int file_cnt = 0;
+int n;
+int has[8] = {0};
+#define a 0
+#define l 1
+#define R 2
+#define t 3
+#define r 4
+#define i 5
+#define s 6
 //-a -l -i -s
-//-a、-l、-R、-t、-r、-i、-s 
+//-a、-l、-R、-t、-r、-i、-s
 
-void myls(const char* dirpath_name);
-void mystat(const char* file_name);
-void print_file_information(char* file_name, struct stat *buf_ptr);
+void myls(const char *dirpath_name);
+void mystat(const char *file_name);
+void print_file_information(char *file_name, struct stat *buf_ptr);
+long long int total(struct stat *buf_ptr);
 
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
-    if(argc == 1){
-        myls(".");
-    }
-    else{
-        while(--argc){
-            myls(*(++argv));
+    // if(argc == 1){
+    //     myls(".");
+    // }
+    // else{
+    //     while(--argc){
+    //         myls(*(++argv));
+    //     }
+    // }
+    // return 0;
+    // char param[100];
+    // int cnt = 0; // 命令个数
+    // int num = 0;
+    // 对命令行参数进行解析，提取到param数组中
+    // for (n = 1; n < argc; n++)
+    // {
+    //     if (argv[n][0] == '-')
+    //     {
+    //         for (int j = 1; j < strlen(argv[i]); cnt++, j++)
+    //         {
+    //             param[cnt] = argv[i][j]; // 获取-后面的参数保存到数组param中
+    //         }
+    //         num++; // 保存 - 的个数
+    //     }
+    // }
+
+    // for (n = 0; n < cnt; n++)
+    // {
+    //     if (param[n] == 'a')has[a]=1;
+    //     else if (param[n] == 'l')has[l]=1;
+    //     else if (param[n] == 'R')has[R]=1;
+    //     else if (param[n] == 't')has[t]=1;
+    //     else if (param[n] == 'r')has[r]=1;
+    //     else if (param[n] == 'i')has[i]=1;
+    //     else if (param[n] == 's')has[s]=1;
+    // }
+
+    char ch;
+    while ((ch = getopt(argc, argv, "alRtris")) != -1)
+    {
+        switch (ch)
+        {
+        case 'a':
+            has[a] = 1;
+            break;
+        case 'l':
+            has[l] = 1;
+            break;
+        case 'R':
+            has[R] = 1;
+            break;
+        case 't':
+            has[t] = 1;
+            break;
+        case 'r':
+            has[r] = 1;
+            break;
+        case 'i':
+            has[i] = 1;
+            break;
+        case 's':
+            has[s] = 1;
+            break;
         }
     }
-    return 0;
+
 }
 
-//打开、读取、关闭目录
-void myls(const char* dirpath_name)
+// 打开、读取、关闭目录
+void myls(const char *dirpath_name)
 {
-    DIR* cur_dir = NULL;
-    struct dirent* cur_item = NULL;
-    //开目录
+    DIR *cur_dir = NULL;
+    struct dirent *cur_item = NULL;
+    // 开目录
     cur_dir = opendir(dirpath_name);
-    if(cur_dir == NULL){
+    if (cur_dir == NULL)
+    {
         printf("目录打开错误!\n");
     }
-    else{
-        //读目录
-        while(1){
+    else
+    {
+        // 读目录
+        while (1)
+        {
             errno = 0;
             cur_item = readdir(cur_dir);
-            if(errno != 0)printf("文件读取错误!\n");
-            if(cur_item == NULL)break;
+            if (errno != 0)
+                printf("文件读取错误!\n");
+            if (cur_item == NULL)
+                break;
             // if(strcmp(cur_item -> d_name,".") == 0 || strcmp(cur_item -> d_name,"..") == 0)continue;//好像没用？
-            if(cur_item -> d_name[0]=='.')continue;//在有-a参数时就不能跳过了
+            if (cur_item->d_name[0] == '.' || cur_item->d_name[0] == '..')
+                continue; // 在有-a参数时就不能跳过了
             restored_ls(cur_item);
         }
-        if()//有什么参数，就。。。
+        sort(filenames, 0, file_cnt - 1);
+        // if()//有什么参数，就。。。
         {
-            mystat(cur_item -> d_name);
+            for (n = 0; n < file_cnt; n++)
+                mystat(filenames[i]);
         }
         printf("\n");
-        //关目录
+        // 关目录
         closedir(cur_item);
     }
 }
 
-//将目录中的文件依次存入数组中
-void restored_ls(struct dirent* cur_item)
+// 将目录中的文件依次存入数组中
+void restored_ls(struct dirent *cur_item)
 {
     filenames[file_cnt++] = cur_item->d_name;
 }
 
-//获取文件具体信息
-void mystat(const char* file_name)
+// 获取文件具体信息
+void mystat(const char *file_name)
 {
     struct stat buf;
-    if(stat(file_name, &buf) == -1)printf("获取文件信息错误!\n");
-    else{
-        if(1){  //如果有-i参数，则执行
+    if (stat(file_name, &buf) == -1)
+        printf("获取文件信息错误!\n");
+    else
+    {
+        if (1)
+        { // 如果有-i参数，则执行
             myls_i(&buf);
         }
-        if(1){  //如果有-s参数，则执行
+        if (1)
+        { // 如果有-s参数，则执行
             myls_s(&buf);
         }
-        print_file_information(file_name,&buf);//注意是&buf
+        print_file_information(file_name, &buf); // 注意是&buf
     }
 }
 
 void myls_i(struct stat *buf_ptr)
 {
-    printf("%d ",buf_ptr -> st_ino);
+    printf("%d ", buf_ptr->st_ino);
 }
 
-//在文件左侧显示文件大小（在-l左边，-i右边），以1024字节为块单位
-//ls -s
-void myls_s(struct stat *buf_ptr)//数据有问题
+// 在文件左侧显示文件大小（在-l左边，-i右边），以1024字节为块单位
+// ls -s
+void myls_s(struct stat *buf_ptr)
 {
-    long long size=buf_ptr -> st_size/1024;                              
-    if(size<=4){           
-        printf("%5d ",4);   
-    }             
-    else{   
-        printf("%5lld ",size);   
-    } 
+    long long int size1 = buf_ptr->st_size;
+    long long int size2 = size1 / 1024;
+    while (size2 % 4 != 0)
+    {
+        size2++;
+    }
+    printf("%5lld ", size2);
 }
 
-
-//打印文件具体信息
-void print_file_information(char* file_name, struct stat *buf_ptr)
+// 总用量
+long long int total(struct stat *buf_ptr)
 {
-    //第一列
+    long long int totall = 0;
+    for (n = 0; n < file_cnt; n++)
+    {
+        long long int size1 = buf_ptr->st_size;
+        long long int size2 = size1 / 1024;
+        while (size2 % 4 != 0)
+        {
+            size2++;
+        }
+        totall += size2;
+    }
+    printf("总用量：%lld", totall);
+}
+
+// //递归调用
+// //ls -R
+// void ls_R(char path[])
+// {
+//   printf("%s:\n",path);
+//   DIR* dir_ptr;
+//   struct dirent* direntp;
+//   if((dir_ptr=opendir(path)) != NULL)//打开目录
+//   {
+//     while((direntp=readdir(dir_ptr)) != NULL)//读取当前目录文件
+//     {
+//       restored_ls(direntp);
+//     }
+//        sort(filenames,0,file_cnt-1);
+//     int j=0;
+//     int i=0;
+//     for(j=0;j<file_cnt;++j)
+//     {
+//       if(has_aRl==1||has_Rl==1)
+//       {
+//           char temp1[PATH_MAX];
+//          sprintf(temp1,"%s/%s",path,filenames[j]);
+//          dostat(temp1,filenames[j]);
+//         continue;
+//       }
+//       if(filenames[j][0]=='.'&&(has_aR!=1&&has_aRl!=1))
+//         continue;
+//       struct stat info;
+//       char temp1[PATH_MAX];
+//       sprintf(temp1,"%s/%s",path,filenames[j]);
+//       if(lstat(temp1,&info)==-1)
+//         perror(temp1);
+//       int color=get_color(info);
+//          if(has_s==1)
+//           {
+//              long long size=info.st_size/1024;
+//                if(size<=4)
+//                   printf("4   ");
+//                else
+//                   printf("%-4lld",size);
+//            }
+//       printf_name2(filenames[j],color);
+//     }
+//   }
+//   printf("\n");
+//   printf("\n");
+//   file_cnt=0;
+//   closedir(dir_ptr);
+//   if((dir_ptr=opendir(path))==NULL)//打开目录
+//     fprintf(stderr,"lsl:cannot open %s\n",path);
+//   else
+//   {
+//     while((direntp=readdir(dir_ptr))!=NULL)
+//     {
+//       if(strcmp(direntp->d_name,".")==0||strcmp(direntp->d_name,"..")==0)
+//         continue;
+//       if(has_R==1)
+//       {
+//         if(direntp->d_name[0]=='.')
+//           continue;
+//       }
+//       struct stat info;
+//       char temp[PATH_MAX];
+//       sprintf(temp,"%s/%s",path,direntp->d_name);
+//       if(lstat(temp,&info)==-1)
+//         perror(temp);
+//       if(S_ISDIR(info.st_mode))//判断是否为目录，如果是目录就进入递归
+//       {
+//         ls_R(temp);
+//       }
+
+//     }
+//   }
+// }
+
+// 打印文件具体信息
+void print_file_information(char *file_name, struct stat *buf_ptr)
+{
+    // 第一列
     char first_column[11] = {0};
-    //第一个字母
-    if ( S_ISLNK(buf_ptr ->st_mode) ) {
-		first_column[0] = 'l';
-	}
-	else if ( S_ISDIR(buf_ptr ->st_mode) ) {
-		first_column[0] = 'd';
-	}
-	else if ( S_ISCHR(buf_ptr ->st_mode) ) {
-		first_column[0] = 'c';
-	}
-	else if ( S_ISSOCK(buf_ptr ->st_mode) ) {
-		first_column[0] = 's';
-	}
-	else if ( S_ISFIFO(buf_ptr ->st_mode) ) {
-		first_column[0] = 'p';
-	}
-	else if ( S_ISBLK(buf_ptr ->st_mode) ) {
-		first_column[0] = 'b';
-	}
-	else {
-		first_column[0] = '-';
-	}
+    // 第一个字母
+    if (S_ISLNK(buf_ptr->st_mode))
+    {
+        first_column[0] = 'l';
+    }
+    else if (S_ISDIR(buf_ptr->st_mode))
+    {
+        first_column[0] = 'd';
+    }
+    else if (S_ISCHR(buf_ptr->st_mode))
+    {
+        first_column[0] = 'c';
+    }
+    else if (S_ISSOCK(buf_ptr->st_mode))
+    {
+        first_column[0] = 's';
+    }
+    else if (S_ISFIFO(buf_ptr->st_mode))
+    {
+        first_column[0] = 'p';
+    }
+    else if (S_ISBLK(buf_ptr->st_mode))
+    {
+        first_column[0] = 'b';
+    }
+    else
+    {
+        first_column[0] = '-';
+    }
 
-    //后面三个为一组
-    //r表示读权限,w表示写权限,x表示可执行权限,-表示无权限
-    //第一组为表示所有者权限
-    first_column[1] = (buf_ptr ->st_mode & S_IRUSR) ? 'r' : '-';
-    first_column[2] = (buf_ptr ->st_mode & S_IWUSR) ? 'w' : '-';
-    first_column[3] = (buf_ptr ->st_mode & S_IXUSR) ? 'x' : '-';
+    // 后面三个为一组
+    // r表示读权限,w表示写权限,x表示可执行权限,-表示无权限
+    // 第一组为表示所有者权限
+    first_column[1] = (buf_ptr->st_mode & S_IRUSR) ? 'r' : '-';
+    first_column[2] = (buf_ptr->st_mode & S_IWUSR) ? 'w' : '-';
+    first_column[3] = (buf_ptr->st_mode & S_IXUSR) ? 'x' : '-';
 
-    //第二组为表示组用户权限
-    first_column[4] = (buf_ptr ->st_mode & S_IRGRP) ? 'r' : '-';
-    first_column[5] = (buf_ptr ->st_mode & S_IWGRP) ? 'w' : '-';
-    first_column[6] = (buf_ptr ->st_mode & S_IXGRP) ? 'x' : '-';
+    // 第二组为表示组用户权限
+    first_column[4] = (buf_ptr->st_mode & S_IRGRP) ? 'r' : '-';
+    first_column[5] = (buf_ptr->st_mode & S_IWGRP) ? 'w' : '-';
+    first_column[6] = (buf_ptr->st_mode & S_IXGRP) ? 'x' : '-';
 
-    //第三组为表示其他用户组的权限
-    first_column[7] = (buf_ptr ->st_mode & S_IROTH) ? 'r' : '-';
-    first_column[8] = (buf_ptr ->st_mode & S_IWOTH) ? 'w' : '-';
-    first_column[9] = (buf_ptr ->st_mode & S_IXOTH) ? 'x' : '-';
+    // 第三组为表示其他用户组的权限
+    first_column[7] = (buf_ptr->st_mode & S_IROTH) ? 'r' : '-';
+    first_column[8] = (buf_ptr->st_mode & S_IWOTH) ? 'w' : '-';
+    first_column[9] = (buf_ptr->st_mode & S_IXOTH) ? 'x' : '-';
 
-    printf("%s ",first_column);
+    printf("%s ", first_column);
 
-    //第二列
-    //目录：表示当前这个目录下面的一级目录个数
-    //文件：表示硬链接个数
-    printf("%d ",(int)buf_ptr -> st_nlink);
+    // 第二列
+    // 目录：表示当前这个目录下面的一级目录个数
+    // 文件：表示硬链接个数
+    printf("%d ", (int)buf_ptr->st_nlink);
 
-    //第三列
-    //该文件或目录的所有者
+    // 第三列
+    // 该文件或目录的所有者
     struct passwd *getpwuid();
     struct passwd *pwuid_ptr;
     static char numstr[10];
-    if((pwuid_ptr = getpwuid(buf_ptr ->st_uid)) == NULL){
-        sprintf(numstr, "%d", buf_ptr ->st_uid);
-        printf("%s ",numstr);
+    if ((pwuid_ptr = getpwuid(buf_ptr->st_uid)) == NULL)
+    {
+        sprintf(numstr, "%d", buf_ptr->st_uid);
+        printf("%s ", numstr);
     }
-    else{
-        printf("%-8s ",pwuid_ptr -> pw_name);//-8向左对齐
+    else
+    {
+        printf("%-8s ", pwuid_ptr->pw_name); //-8向左对齐
     }
 
-    //第四列
-    //表示所属的组
+    // 第四列
+    // 表示所属的组
     struct group *getgrgid();
     struct group *group_ptr;
     static char num[10];
-    if((group_ptr = getgrgid(buf_ptr ->st_gid)) == NULL){
-        sprintf(num, "%d", buf_ptr ->st_gid);
-        printf("%s ",num);
-    }else{
-        printf("%-8s ",group_ptr -> gr_name);//-8向左对齐
+    if ((group_ptr = getgrgid(buf_ptr->st_gid)) == NULL)
+    {
+        sprintf(num, "%d", buf_ptr->st_gid);
+        printf("%s ", num);
+    }
+    else
+    {
+        printf("%-8s ", group_ptr->gr_name); //-8向左对齐
     }
 
-    //第五列
-    //文件大小
-    printf("%8ld ",(long)buf_ptr -> st_size);//8ld向右对齐
+    // 第五列
+    // 文件大小
+    printf("%8ld ", (long)buf_ptr->st_size); // 8ld向右对齐
 
-    //第六、七、八列
-    //月、日、时
-    char *time = ctime(&(buf_ptr -> st_mtime));
+    // 第六、七、八列
+    // 月、日、时
+    char *time = ctime(&(buf_ptr->st_mtime));
     char file_time[512] = {0};
     strncpy(file_time, time, strlen(time) - 1);
-    printf("%12.12s ",4+file_time);//不用星期，星期占前4地址,12.12为截取日期长度为12
+    printf("%12.12s ", 4 + file_time); // 不用星期，星期占前4地址,12.12为截取日期长度为12
 
-    //第九列
-    //文件名
-    printf("%s\n",file_name);
-
+    // 第九列
+    // 文件名
+    printf("%s\n", file_name);
 }
 
-//排序
-void sort(char** filenames,int start,int end)
+// 排序
+void sort(char **filenames, int start, int end)
 {
-    if(start < end){
-        int position = partition(filenames,start,end);
-        sort(filenames,start,position - 1);
-        sort(filenames,position + 1,end);
+    if (start < end)
+    {
+        int position = partition(filenames, start, end);
+        sort(filenames, start, position - 1);
+        sort(filenames, position + 1, end);
     }
 }
 
-//比较两字符串的字典序
-int compare(char* s1,char* s2)
+// 比较两字符串的字典序
+int compare(char *s1, char *s2)
 {
-    if(*s1=='.')s1++;
-    if(*s2=='.')s2++;
-    while(*s1 && *s2 && *s1 == *s2){
+    if (*s1 == '.')
+        s1++;
+    if (*s2 == '.')
+        s2++;
+    while (*s1 && *s2 && *s1 == *s2)
+    {
         ++s1;
         ++s2;
-    if(*s1=='.')s1++;
-    if(*s2=='.')s2++;
+        if (*s1 == '.')
+            s1++;
+        if (*s2 == '.')
+            s2++;
     }
-    return *s1 - *s2;//s1靠前，返回负数，s1靠后，返回正数；s1和s2完全一样，返回0
+    return *s1 - *s2; // s1靠前，返回负数，s1靠后，返回正数；s1和s2完全一样，返回0
 }
 
-//交换两字符串
-void swap(char** s1,char** s2)
+// 交换两字符串
+void swap(char **s1, char **s2)
 {
-    char* temp = *s1;
+    char *temp = *s1;
     *s1 = *s2;
     *s2 = temp;
 }
 
-int partition(char** filenames,int start,int end)
+int partition(char **filenames, int start, int end)
 {
-    char* privot = filenames[start];
-    while(start < end){
-        while(start < end && compare(privot,filenames[end]) < 0)
+    char *privot = filenames[start];
+    while (start < end)
+    {
+        while (start < end && compare(privot, filenames[end]) < 0)
             --end;
-        swap(&filenames[start],&filenames[end]);
-        while(start < end && compare(privot,filenames[start]) >= 0)
+        swap(&filenames[start], &filenames[end]);
+        while (start < end && compare(privot, filenames[start]) >= 0)
             ++start;
-        swap(&filenames[start],&filenames[end]);
+        swap(&filenames[start], &filenames[end]);
     }
     return start;
 }
-
