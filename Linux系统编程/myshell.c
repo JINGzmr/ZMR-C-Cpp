2a91bf94cc43e060fb5d1be2997265a04c3589a8
JINGzmr <2743377630@qq.com>
2023-03-19 22:41:25 +0800

    3.19

--- Linux系统编程/myshell.c
+++ Linux系统编程/myshell.c
@@ -6,7 +6,7 @@
 #include <string.h> // strsep()
 #include <sys/wait.h>
 
-#define DELIMS " \t\n" //strsep要的分割符，包括空格
+#define DELIMS " \t\n" // strsep要的分割符，包括空格
 
 struct cmd_st
 {
@@ -14,8 +14,7 @@ struct cmd_st
 };
 
 void prompt(void);
-static void parse(char *line, struct cmd_st *res);  //将一段文本或数据解析成特定格式
-
+static void parse(char *line, struct cmd_st *res); // 将一段文本或数据解析成特定格式
 
 int main()
 {
@@ -29,17 +28,17 @@ int main()
         prompt(); // 打印提示符
 
         if (getline(&linebuf, &linebuf_size, stdin) < 0) // 从终端获取用户的输入  stdin->从标准输入来读
-            break;                                      //getline()从流中读取整行，将包含文本的缓冲区地址存储到*linebuf中
+            break;                                       // getline()从流中读取整行，将包含文本的缓冲区地址存储到*linebuf中
 
-        parse(linebuf,&cmd); // 解析前面获取的那一行命令（->判断是内部命令还是外部命令）
+        parse(linebuf, &cmd); // 解析前面获取的那一行命令（->判断是内部命令还是外部命令）
 
         if (0) // 是内部命令 ->先不管内部的
         {
-            //do something
+            // do something
         }
         else // 是外部命令
         {
-            fork();
+            pid = fork();
             if (pid < 0)
             {
                 perror("fork()");
@@ -48,13 +47,13 @@ int main()
 
             if (pid == 0) // child
             {
-                execvp(cmd.globres.gl_pathv[0],cmd.globres.gl_pathv);
+                execvp(cmd.globres.gl_pathv[0], cmd.globres.gl_pathv);
                 perror("execvp()");
                 exit(1);
             }
             else // parent
             {
-                wait(NULL);  //收尸完再进行循环
+                wait(NULL); // 收尸完再进行循环
             }
         }
     }
@@ -62,27 +61,25 @@ int main()
     exit(0);
 }
 
-
 void prompt(void)
 {
     printf("mysh-0.1$ ");
 }
 
-
-static void parse(char *line, struct cmd_st *res)  //将一段文本或数据解析成特定格式
-{ // ls -a -l   /etc  .....
-    char *tok;  //strsep要的返回值是一个char*类型，存每次while循环扣出来的小串
+static void parse(char *line, struct cmd_st *res) // 将一段文本或数据解析成特定格式
+{                                                 // ls -a -l   /etc  .....
+    char *tok;                                    // strsep要的返回值是一个char*类型，存每次while循环扣出来的小串
     int i = 0;
 
-    while (1)//循环解析这些命令
+    while (1) // 循环解析这些命令
     {
-        tok = strsep(&line, DELIMS);  //strtok:将一个字符串分解为0个或多个非空标记的序列
+        tok = strsep(&line, DELIMS); // strtok:将一个字符串分解为0个或多个非空标记的序列
         if (tok == NULL)
             break;
-        if (tok[0] == '\0') //解析出来是一个空串
+        if (tok[0] == '\0') // 解析出来是一个空串
             continue;
 
-        glob(tok, GLOB_NOCHECK | GLOB_APPEND * i, NULL, &res->globres); //把结果带回去
+        glob(tok, GLOB_NOCHECK | GLOB_APPEND * i, NULL, &res->globres); // 把结果带回去
         i = 1;
     }
 }
