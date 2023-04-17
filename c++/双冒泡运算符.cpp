869d849e87b2827d907995fc72104d04f0a25e97
JINGzmr <2743377630@qq.com>
2023-04-17 12:37:23 +0800

    .

--- c++/双冒泡运算符.cpp
+++ c++/双冒泡运算符.cpp
@@ -1,9 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+int atk = 1000;
+
+void test01()
+{
+    int atk = 2000;
+    cout << "atk = " << atk << endl;
+
+    //::代表作用域  如果前面什么都不加，则代表全局作用域
+    cout << "atk = " << ::atk << endl;
+}
+
 int main()
 {
-
-
+    test01();
 
     system("pause");
     return 0;
