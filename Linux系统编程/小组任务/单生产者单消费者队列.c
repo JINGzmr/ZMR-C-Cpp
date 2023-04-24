f46488a50789782700361fce764a734dd1e4ae9a
JINGzmr <2743377630@qq.com>
2023-04-24 21:59:36 +0800

    4.24

--- Linux系统编程/小组任务/单生产者单消费者队列.c
+++ Linux系统编程/小组任务/单生产者单消费者队列.c
@@ -14,8 +14,8 @@ struct SPSCQueue
 } typedef SPSCQueue;
 
 SPSCQueue *SPSCQueueInit(int capacity);        // 初始化一个 SPSC 队列，并返回一个指向 SPSCQueue 结构体的指针。capacity: 队列可以存储的元素数量的最大值。
-void SPSCQueuePush(SPSCQueue *queue, void *s){} // 将一个指向 void 类型的数据指针 s 推入队列中
-void *SPSCQueuePop(SPSCQueue *queue){}          // 从队列中弹出一个数据指针
+void SPSCQueuePush(SPSCQueue *queue, void *s); // 将一个指向 void 类型的数据指针 s 推入队列中
+void *SPSCQueuePop(SPSCQueue *queue);          // 从队列中弹出一个数据指针
 void SPSCQueueDestory(SPSCQueue *);            // 销毁一个 SPSC 队列，并释放相关的内存资源
 
 int main()
@@ -24,8 +24,16 @@ int main()
     pthread_t pid, cid; // 定义生产者和消费者的线程ID
     srand(time(NULL));  // 设置随机数生成器的种子，时之后每次获得的随机数更随即
 
-    pthread_create(&pid, NULL, &SPSCQueuePush, NULL); // 创建生产者线程
-    pthread_create(&cid, NULL, SPSCQueuePop, NULL);   // 创建消费者线程
+    int ret1 = pthread_create(&pid, NULL, (void*)&SPSCQueuePush, NULL); // 创建生产者线程
+    int ret2 = pthread_create(&cid, NULL, (void*)SPSCQueuePop, NULL);   // 创建消费者线程
+
+    //检查线程是否创建成功
+    if(ret1!=0){  
+        err_thread(ret1, "pthread_create produser error");
+    }
+    if(ret2!=0){
+        err_thread(ret2,"pthead_create consuer error");
+    }
 
     pthread_join(pid, NULL); // 回收生产者线程
     pthread_join(cid, NULL); // 回收消费者线程
