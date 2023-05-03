84d51272f261809d833ae7e18a5deff62a13ce56
JINGzmr <2743377630@qq.com>
2023-05-03 22:25:22 +0800

    5.3 哲学家

---
+++ Linux系统编程/小组任务/线程/哲学家问题（1）.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<pthread.h>'
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
+
+int rice = 100;
+pthread_mutex_t ch[5];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *eat(void *arg);
+
+int main()
+{
+    int i;
+    for(i=0;i<5;i++){
+        pthread_mutex_init(&ch[i],NULL);
+    }
+
+    pthread_t threads_pid[5];
+    for(i=0;i<5;i++){
+        pthread_create(&threads_pid[i], NULL, eat, i);
+    }
+
+    for(i=0;i<5;i++){
+        pthread_join(threads_pid[i], NULL);
+    }
+
+    return 0;
+}
+
+void *eat(void *arg)
+{
+    int id = (int)arg;
+
+    while(1)
+    {
+        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&ch[id]);
+        pthread_mutex_lock(&ch[(id+1)%5]);
+
+        pthread_mutex_unlock(&mutex);
+
+        printf("%d号哲学家正在吃%d\n",id,rice--);
+        sleep(1); //eat
+
+        pthread_mutex_unlock(&ch[id]);
+        pthread_mutex_unlock(&ch[(id+1)%5]);
+    }
+
+
+}
