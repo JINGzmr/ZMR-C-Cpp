6b35ce1618d7f83e9676375eb6c2584a7ddec692
JINGzmr <jingzmr@163.com>
2024-04-26 11:36:28 +0800

    learning go

--- go/learning.go
+++ go/learning.go
@@ -9,52 +9,97 @@
 
 // }
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func main() {
+
+// 	i := 2
+// 	fmt.Print("write ", i, " as ")
+// 	switch i {
+// 	case 1:
+// 		fmt.Println("one")
+// 	case 2:
+// 		fmt.Println("two")
+// 	case 3:
+// 		fmt.Println("three")
+// 	}
+
+// 	switch time.Now().Weekday() {
+// 	case time.Saturday, time.Sunday:
+// 		fmt.Println("It's the weekend")
+// 	default:
+// 		fmt.Println("It's a weekday")
+// 	}
+
+// 	t := time.Now()
+// 	switch {
+// 	case t.Hour() < 12:
+// 		fmt.Println("It's before noon")
+// 	default:
+// 		fmt.Println("It's after noon")
+// 	}
+
+// 	whatAmI := func(i interface{}) {
+// 		switch t := i.(type) {
+// 		case bool:
+// 			fmt.Println("I'm a bool")
+// 		case int:
+// 			fmt.Println("I'm an int")
+// 		default:
+// 			fmt.Printf("Don't know type %T\n", t)
+// 		}
+// 	}
+// 	whatAmI(true)
+// 	whatAmI(1)
+// 	whatAmI("hey")
+// }
+
+// int func(int a,int b){
+
+// }
+
+// func myfunc(int a,int b)int{
+
+// }
+
 package main
 
 import (
 	"fmt"
-	"time"
+	// "string"
+	// "time"
 )
 
 func main() {
+	// var a, b int
+	// fmt.Scan(&a, &b)
+	// fmt.Println(a, b)
 
-	i := 2
-	fmt.Print("write ", i, " as ")
-	switch i {
-	case 1:
-		fmt.Println("one")
-	case 2:
-		fmt.Println("two")
-	case 3:
-		fmt.Println("three")
-	}
+	// num := 0
+	// var arr [2][2]int
+	// for i := 0; i < 2; i++ {
+	// 	for j := 0; j < 2; j++ {
+	// 		num++
+	// 		arr[i][j] = num
+	// 		// fmt.Println(num)
+	// 	}
+	// }
 
-	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("It's the weekend")
-	default:
-		fmt.Println("It's a weekday")
-	}
+	// fmt.Println(arr)
 
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("It's before noon")
-	default:
-		fmt.Println("It's after noon")
-	}
+	// s := make([]string, 4)
+	// // a := 2
+	// s[1] = "a"
+	// s[2] = "b"
+	// s[3] = "c"
+	// l := s[:]
+	// fmt.Println(l)
 
-	whatAmI := func(i interface{}) {
-		switch t := i.(type) {
-		case bool:
-			fmt.Println("I'm a bool")
-		case int:
-			fmt.Println("I'm an int")
-		default:
-			fmt.Printf("Don't know type %T\n", t)
-		}
-	}
-	whatAmI(true)
-	whatAmI(1)
-	whatAmI("hey")
+	m := make(map[string]int)
+
 }
